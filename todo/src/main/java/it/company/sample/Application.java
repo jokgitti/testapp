b3e6d26d3042186ed4c12462ced778b60e8dc2a4
Luca Galeazzi <jokgitti@gmail.com>
2016-03-15 10:34:59 +0100

    no message

--- todo/src/main/java/it/company/sample/Application.java
+++ todo/src/main/java/it/company/sample/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
